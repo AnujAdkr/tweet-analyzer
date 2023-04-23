72ce09daf6912739b823c350364f48ee421a0fbc
anujadhikary <aadhikary@spendlabs.com>
2023-04-23 08:42:18 +0530

    Initial commit of spring boot application

---
+++ src/main/java/com/mysolution/tweetanalyzer/TweetanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.mysolution.tweetanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysolution/tweetanalyzer/TweetanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysolution.tweetanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
